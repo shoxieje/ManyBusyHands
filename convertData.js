810818d0049c86ac4a1e8e4f008d8d1c0d9e162a
chandradewi26 <69431090+chandradewi26@users.noreply.github.com>
2023-01-17 02:49:39 +1100

    Manage job page design fix

--- convertData.js
+++ convertData.js
@@ -1,80 +1,76 @@
+const { json } = require("express");
+const fs = require("fs");
+const readline = require("readline");
+const axios = require("axios");
 
-const { json } = require('express');
-const fs = require('fs');
-const readline = require('readline');
-const axios = require('axios');
-
-
-
-
-let activity = []
+let activity = [];
 
 async function processByLine() {
-
-    const fileStream = fs.createReadStream('../farm_business.txt');
+    const fileStream = fs.createReadStream("farm_business.txt");
 
     const rl = readline.createInterface({
-
         input: fileStream,
-        crlfDelay: Infinity
-        
+        crlfDelay: Infinity,
     });
 
     for await (const line of rl) {
-        
         let tempActivities = [];
 
-        if(line.includes('/')) {
-            tempActivities = line.split('/');
+        if (line.includes("/")) {
+            tempActivities = line.split("/");
 
-            for(let x of tempActivities) {
-
+            for (let x of tempActivities) {
                 let xx = x.trim().split(" ");
 
-                if(xx.length >= 2) {
-                    if((xx[0].startsWith("Au") || xx[0].startsWith["au"]) && (xx[1].startsWith("S") || xx[1].startsWith("s"))) {
-                        x = "Australian Stock Horse"
+                if (xx.length >= 2) {
+                    if (
+                        (xx[0].startsWith("Au") || xx[0].startsWith["au"]) &&
+                        (xx[1].startsWith("S") || xx[1].startsWith("s"))
+                    ) {
+                        x = "Australian Stock Horse";
                     }
                 }
 
-                addToActivity(x.trim())
+                addToActivity(x.trim());
             }
         } else {
-            addToActivity(line)
+            addToActivity(line);
         }
     }
 
     console.log(activity.length);
-    
-    for(const x of activity) {
 
-        await axios.post(`http://localhost:8081/activity`, new URLSearchParams({
-            activityName: x
-        }), {
-            'Accept': 'application/x-www-form-urlencoded',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },)
-
+    for (const x of activity) {
+        await axios.post(
+            `http://localhost:8081/activity`,
+            new URLSearchParams({
+                activityName: x,
+            }),
+            {
+                Accept: "application/x-www-form-urlencoded",
+                "Content-Type": "application/x-www-form-urlencoded",
+            }
+        );
     }
 
-
-    
     // let json_activity = JSON.stringify(activity);
     // fs.writeFileSync('dataField.json', json_activity);
 }
 
 function addToActivity(value) {
-    
-    const splitWords = value.split(" ").filter(e => {
-        return e !== '' && e !== '.'
-    }).map((word) => {
-        return word[0].toUpperCase() + word.substring(1);
-    }).join(" ");
+    const splitWords = value
+        .split(" ")
+        .filter((e) => {
+            return e !== "" && e !== ".";
+        })
+        .map((word) => {
+            return word[0].toUpperCase() + word.substring(1);
+        })
+        .join(" ");
 
-    if(!activity.includes(splitWords)) {
-        activity.push(splitWords)
+    if (!activity.includes(splitWords)) {
+        activity.push(splitWords);
     }
-
 }
 
 processByLine();
